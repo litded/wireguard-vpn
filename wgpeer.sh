eb3149bb0772c14b07276bee067bea3bf001bd05
litded <l@itded.ru>
2022-03-20 17:28:10 +0300

    update

---
+++ wgpeer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "must pass a client name as an arg: add-client.sh new-client"
+else
+	echo "Creating client config for: $1"
+	mkdir -p clients/$1
+	wg genkey | tee clients/$1/$1.priv | wg pubkey > clients/$1/$1.pub
+	key=$(cat clients/$1/$1.priv) 
+	ip="10.200.10."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
+	FQDN=$(hostname -f)
+  SERVER_PUB_KEY=$(cat /etc/wireguard/publickey)
+  cat wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$SERVER_PUB_KEY"'|' | sed -e 's|:SERVER_IP:|'"$(wget -O - -q icanhazip.com)"'|' > clients/$1/wg0.conf
+	echo $ip > last-ip.txt
+	tar czvf clients/$1.tar.gz clients/$1
+	echo "Created config!"
+	echo "Adding peer"
+	sudo wg set wg0 peer $(cat clients/$1/$1.pub) allowed-ips $ip/32
+	echo "Adding peer to hosts file"
+	echo $ip" "$1 | sudo tee -a /etc/hosts
+	sudo wg show
+	qrencode -t ansiutf8 < clients/$1/wg0.conf
+fi
